54e3dcfbacc66ab12cfdf5f8b5c1172802358316
M-Orxan <orxanm385@gmail.com>
2023-02-07 16:44:23 +0400

    Update operation

--- To Do List/To Do List/Controllers/HomeController.cs
+++ To Do List/To Do List/Controllers/HomeController.cs
@@ -15,12 +15,14 @@ namespace To_Do_List.Controllers
             _db = db;
         }
 
+        //Index
         public async Task<IActionResult> Index()
         {
-            List<TaskModel> tasks = await _db.Tasks.Where(x=>x.IsDeactive==false).ToListAsync();
+            List<TaskModel> tasks = await _db.Tasks.Where(x => x.IsDeactive == false).ToListAsync();
             return View(tasks);
         }
 
+        //Create
         public IActionResult Create()
         {
             return View();
@@ -31,15 +33,8 @@ namespace To_Do_List.Controllers
 
         public async Task<IActionResult> Create(TaskModel task)
         {
-
-            if (task.Title == null)
-            {
-                ModelState.AddModelError("Title","");
-                return View(task);
-            }
-
-
-            bool isExists = await _db.Tasks.Where(x=>x.IsDeactive==false).AnyAsync(x => x.Title == task.Title);
+            task.Title = task.Title?.Trim();
+            bool isExists = await _db.Tasks.Where(x => x.IsDeactive == false).AnyAsync(x => x.Title == task.Title);
 
             if (isExists)
             {
@@ -54,6 +49,7 @@ namespace To_Do_List.Controllers
 
             }
 
+            //task.Title = task.Title?.Trim();
             task.Created = DateTime.Now;
             task.InProgress = true;
             task.IsDeactive = false;
@@ -65,9 +61,7 @@ namespace To_Do_List.Controllers
         }
 
 
-
-
-
+        //Delete
         public async Task<IActionResult> Delete(int id)
         {
             if (id == 0)
@@ -75,7 +69,7 @@ namespace To_Do_List.Controllers
                 return NotFound();
             }
 
-            TaskModel? task = await _db.Tasks.FirstOrDefaultAsync(x=>x.Id==id);
+            TaskModel? task = await _db.Tasks.FirstOrDefaultAsync(x => x.Id == id);
             if (task == null)
             {
                 return BadRequest();
@@ -89,7 +83,7 @@ namespace To_Do_List.Controllers
             return RedirectToAction("Index");
         }
 
-
+        //Detail
         public async Task<IActionResult> Detail(int id)
         {
             if (id == 0)
@@ -104,6 +98,75 @@ namespace To_Do_List.Controllers
             }
 
             return View(task);
+        }
+
+        //Update
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            TaskModel? task = await _db.Tasks.FirstOrDefaultAsync(x => x.Id == id);
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+
+            return View(task);
+        }
+
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+
+        public async Task<IActionResult> Update(int id, TaskModel task)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            TaskModel? dbTask = await _db.Tasks.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbTask == null)
+            {
+                return BadRequest();
+            }
+
+            task.Title=task.Title?.Trim();
+            bool isExists = await _db.Tasks.Where(x => x.IsDeactive == false).AnyAsync(x => x.Title == task.Title && x.Id != id);
+
+            if (isExists)
+            {
+                ModelState.AddModelError("Title", "There is already note under this title. Please change the title");
+                return View(task);
+            }
+
+
+
+            if (task.Deadline < DateTime.Now)
+            {
+                ModelState.AddModelError("Deadline", "Deadline can not be earlier than current time");
+                return View(task);
+
+            }
+
+            if (task.InProgress == false && task.Completed == false)
+            {
+                ModelState.AddModelError("InProgress", "Status must be either InProgress or Completed");
+                return View(task);
+            }
+
+            dbTask.Title = task.Title;
+            dbTask.Description = task.Description;
+            dbTask.Deadline = task.Deadline;
+            dbTask.Completed = task.Completed;
+            dbTask.InProgress = task.InProgress;
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
 
